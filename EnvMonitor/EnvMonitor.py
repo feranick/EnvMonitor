fdae3fa87f00a0b0273e29cc5539dc452110f9b4
Nicola Ferralis <feranick@hotmail.com>
2019-03-03 17:18:09 -0500

    Major revamp based on DataSubmitter.

--- EnvMonitor/EnvMonitor.py
+++ EnvMonitor/EnvMonitor.py
@@ -3,8 +3,8 @@
 '''
 **********************************************************
 *
-* GridEdge - Environmental Tracking - using classes
-* version: 20170824a
+* GridEdge - Environmental Tracking
+* version: 20190303a
 *
 * By: Nicola Ferralis <feranick@hotmail.com>
 *
@@ -13,34 +13,29 @@
 #print(__doc__)
 
 import sys, math, json, os.path, time
+from pymongo import MongoClient
 from Adafruit_BME280 import *
 import RPi.GPIO as GPIO
 
-global MongoDBhost
-
 def main():
-    if len(sys.argv)<3 or os.path.isfile(sys.argv[2]) == False:
-        print(__doc__)
-        print(' Usage:\n  python3 GridEdge_EnvMonitor_class.py <lab-identifier> <mongoFile>\n')
-        return
-    
-    lab = sys.argv[1]
-    mongoFile = sys.argv[2]
+    #if len(sys.argv)<3 or os.path.isfile(sys.argv[2]) == False:
+    #    print(__doc__)
+    #    print(' Usage:\n  python3 EnvMonitor.py\n')
+    #    return
     
     #************************************
-    ''' Read from T/RH sensor '''
+    ''' NEW: Read from T/RH sensor '''
     #************************************
     trhSensor = TRHSensor(lab)
     sensData = trhSensor.readSensors()
     trhSensor.printUI()
-    
-    #************************************
-    ''' Make JSON and push to MongoDB '''
-    #************************************
-    conn = GEmongoDB(sensData,mongoFile)
-    print("\n JSON:\n",conn.makeJSON(),"\n")
-    print(" Pushing to MongoDB:")
-    conn.pushToMongoDB()
+    jsonData, sub = trhSensor.makeJson()
+    try:
+            conn = SubMongoDB(sensData)
+            #conn.checkCreateLotDM(sub)
+            conn.pushToMongoDB()
+        except:
+            print("\n Submission to database failed!\n")
 
 #************************************
 ''' Class T/RH Sensor '''
@@ -63,10 +58,19 @@ class TRHSensor:
             self.sensData.extend([sensor.read_temperature(),
                                   sensor.read_pressure() / 100,
                                   sensor.read_humidity()])
+            dataj = {
+            'lab' : self.sensData[0],
+            'IP' : self.sensData[1],
+            'date' : self.sensData[2],
+            'time' : self.sensData[3],
+            'temperature' : '{0:0.1f}'.format(self.sensData[4]),
+            'pressure' : '{0:0.1f}'.format(self.sensData[5]),
+            'humidity' : '{0:0.1f}'.format(self.sensData[6]),
+            }
         except:
             print("\n SENSOR NOT CONNECTED ")
-            self.sensData.extend([0.0,0.0,0.0])
-        return self.sensData
+            dataj = self.sensData.extend([0.0,0.0,0.0])
+        return json.dumps(dataj)
 
     #************************************
     ''' Print Values on screen '''
@@ -80,59 +84,211 @@ class TRHSensor:
         print(" Pressure = {0:0.1f} hPa".format(self.sensData[5]))
         print(" Humidity = {0:0.1f} %".format(self.sensData[6]),"\n")
 
+
 #************************************
 ''' Class Database '''
 #************************************
-class GEmongoDB:
-    def __init__(self, data, file):
-        self.data = data
-        with open(file, 'r') as f:
-            f.readline()
-            self.hostname = f.readline().rstrip('\n')
-            f.readline()
-            self.port_num = f.readline().rstrip('\n')
-            f.readline()
-            self.dbname = f.readline().rstrip('\n')
-            f.readline()
-            self.username = f.readline().rstrip('\n')
-            f.readline()
-            self.password = f.readline().rstrip('\n')
+class SubMongoDB:
+    def __init__(self, jsonData):
+        self.config = Configuration()
+        self.config.readConfig(self.config.configFile)
+        if self.config.dataType == 0:
+            self.jsonData = jsonData
+        else:
+            self.jsonData = json.loads(jsonData)
 
     def connectDB(self):
-        from pymongo import MongoClient
-        client = MongoClient(self.hostname, int(self.port_num))
-        auth_status = client[self.dbname].authenticate(self.username, self.password, mechanism='SCRAM-SHA-1')
-        print(' Authentication status = {0} \n'.format(auth_status))
+        client = MongoClient(self.config.DbHostname, int(self.config.DbPortNumber))
+        auth_status = client[self.config.DbName].authenticate(self.config.DbUsername,self.config.DbPassword)
+        print("\n Pushing to MongoDB: Authentication status = {0}".format(auth_status))
         return client
 
     def printAuthInfo(self):
-        print(self.hostname)
-        print(self.port_num)
-        print(self.dbname)
-        print(self.username)
-        print(self.password)
-
-    def makeJSON(self):
-        dataj = {
-            'lab' : self.data[0],
-            'IP' : self.data[1],
-            'date' : self.data[2],
-            'time' : self.data[3],
-            'temperature' : '{0:0.1f}'.format(self.data[4]),
-            'pressure' : '{0:0.1f}'.format(self.data[5]),
-            'humidity' : '{0:0.1f}'.format(self.data[6]),
-            }
-        return json.dumps(dataj)
+        print(self.config.DbHostname)
+        print(self.config.DbPortNumber)
+        print(self.config.DbName)
+        print(self.config.DbUsername)
+        print(self.config.DbPassword)
     
     def pushToMongoDB(self):
-        jsonData = self.makeJSON()
         client = self.connectDB()
-        db = client[self.dbname]
+        db = client[self.config.DbName]
         try:
-            db_entry = db.EnvTrack.insert_one(json.loads(jsonData))
+            db_entry = db.Measurement.insert_one(self.jsonData)
             print(" Data entry successful (id:",db_entry.inserted_id,")\n")
         except:
             print(" Data entry failed.\n")
+    '''
+    # View entry in DM page for substrate/device
+    def checkCreateLotDM(self, deviceID):
+        client = self.connectDB()
+        db = client[self.config.DbName]
+        #try:
+        entry = db.Lot.find_one({'label':deviceID[:8]})
+        if entry:
+            #db.Lot.update_one({ '_id': entry['_id'] },{"$push": self.getArchConfig(deviceID, row, col)}, upsert=False)
+            msg = " Data entry for this batch found in DM. Created substrate: "+deviceID
+        else:
+            print(" No data entry for this substrate found in DM. Creating new one...")
+            jsonData = {'label' : deviceID[:8], 'date' : deviceID[2:8], 'description': '', 'notes': '', 'tags': [], 'substrates': [{'isCollapsed': False, 'label': deviceID, 'material': '', 'flex': False, 'area': '', 'layers': [], 'attachments': [], 'devices': [{'size': '', 'measurements': []}, {'size': '', 'measurements': []}, {'size': '', 'measurements': []}, {'size': '', 'measurements': []}, {'size': '', 'measurements': []}, {'size': '', 'measurements': []}]}]}
+            db_entry = db.Lot.insert_one(json.loads(json.dumps(jsonData)))
+                #db.Lot.update_one({ '_id': db_entry.inserted_id },{"$push": self.getArchConfig(deviceID,row,col)}, upsert=False)
+            msg = " Created batch: " + deviceID[:8] + " and device: "+deviceID
+        print(msg)
+    
+        
+        #except:
+        #    print(" Connection with DM via Mongo cannot be established.")
+    '''
+
+    def getById(self, id):
+        from bson.objectid import ObjectId
+        client = self.connectDB()
+        db = client[self.config.DbName]
+        db_entry = db.EnvMon.find_one({"_id": ObjectId(id)})
+        print("\n Restoring file: :",db_entry['file'][2:])
+        with open(db_entry['file'][2:], "wb") as fh:
+            fh.write(base64.b64decode(db_entry[self.config.headers[0]]))
+
+    def getByFile(self, file):
+        client = self.connectDB()
+        db = client[self.config.DbName]
+        db_entry = db.EnvMon.find_one({"file": "./"+file})
+        print("\n Restoring file: :",db_entry['file'][2:])
+        with open(db_entry['file'][2:], "wb") as fh:
+            fh.write(base64.b64decode(db_entry[self.config.headers[0]]))
+
+####################################################################
+# Configuration
+####################################################################
+class Configuration():
+    def __init__(self):
+        self.home = str(Path.home())+"/"
+        #self.home = str(Path.cwd())+"/"
+        self.configFile = self.home+"EnvMonitor.ini"
+        self.generalFolder = self.home+"EnvMonitor/"
+        #Path(self.generalFolder).mkdir(parents=True, exist_ok=True)
+        self.logFile = self.generalFolder+"EnvmoNitor.log"
+        self.conf = configparser.ConfigParser()
+        self.conf.optionxform = str
+    
+    # Create configuration file
+    def createConfig(self):
+        try:
+            self.defineSystem()
+            self.defineInstrumentation()
+            self.defineData()
+            self.defineConfDM()
+            with open(self.configFile, 'w') as configfile:
+                self.conf.write(configfile)
+        except:
+            print("Error in creating configuration file")
+
+    # Hadrcoded default definitions for the confoguration file
+    def defineSystem(self):
+        self.conf['System'] = {
+            'appVersion' : 0,
+            'loggingLevel' : logging.INFO,
+            'loggingFilename' : self.logFile,
+            'dataFolder' : ".",
+            }
+    def defineInstrumentation(self):
+        self.conf['Instrumentation'] = {
+            'measType' : 'test',
+            'equipment' : 'test',
+            'name' : 'test',
+            'itemId' : '1'
+            }
+    '''
+    # for images/binary
+    def defineData(self):
+        self.conf['Data'] = {
+            'headers' : ['image'],
+            'encoding' : 'base64.b64encode',
+            'dataType' : 0,
+            'ncols': [0,1],
+            }
+    
+    # for text/ASCII
+    def defineData(self):
+        self.conf['Data'] = {
+            'headers' : ['header0','header1'],
+            'encoding' : 'text/ASCII',
+            'dataType' : 1,
+            'ncols': [0,1],
+            }
+    
+    # for text/csv
+    def defineData(self):
+        self.conf['Data'] = {
+            'headers' : ['X','Y'],
+            'encoding' : 'text/CSV',
+            'dataType' : 1,
+            'ncols': [0,1],
+            'extension' : 'txt',
+            }
+    '''
+    def defineConfDM(self):
+        self.conf['DM'] = {
+            'DbHostname' : "my.site.com",
+            'DbPortNumber' : "27017",
+            'DbName' : "test",
+            'DbUsername' : "user",
+            'DbPassword' : "pwd",
+            }
+
+    # Read configuration file into usable variables
+    def readConfig(self, configFile):
+        self.conf.read(configFile)
+        self.sysConfig = self.conf['System']
+        self.appVersion = self.sysConfig['appVersion']
+        try:
+            self.instrumentationConfig = self.conf['Instrumentation']
+            #self.dataConfig = self.conf['Data']
+            self.dmConfig = self.conf['DM']
+
+            self.loggingLevel = self.sysConfig['loggingLevel']
+            self.loggingFilename = self.sysConfig['loggingFilename']
+            self.dataFolder = self.sysConfig['dataFolder']
+
+            self.equipment = self.instrumentationConfig['equipment']
+            self.name = self.instrumentationConfig['name']
+            self.measType = self.instrumentationConfig['measType']
+            #self.architecture = self.instrumentationConfig['architecture']
+            self.itemId = self.instrumentationConfig['itemId']
+            
+            '''
+            self.headers = eval(self.dataConfig['headers'])
+            self.dataType = eval(self.dataConfig['dataType'])
+            self.encoding = self.dataConfig['encoding']
+            self.ncols = eval(self.dataConfig['ncols'])
+            self.extension = self.dataConfig['extension']
+            '''
+            
+            self.DbHostname = self.dmConfig['DbHostname']
+            self.DbPortNumber = self.conf.getint('DM','DbPortNumber')
+            self.DbName = self.dmConfig['DbName']
+            self.DbUsername = self.dmConfig['DbUsername']
+            self.DbPassword = self.dmConfig['DbPassword']
+        
+        except:
+            print("Configuration file is for an earlier version of the software")
+            oldConfigFile = str(os.path.splitext(configFile)[0] + "_" +\
+                    str(datetime.now().strftime('%Y%m%d-%H%M%S'))+".ini")
+            print("Old config file backup: ",oldConfigFile)
+            os.rename(configFile, oldConfigFile )
+            print("Creating a new config file.")
+            self.createConfig()
+            self.readConfig(configFile)
+
+    # Save current parameters in configuration file
+    def saveConfig(self, configFile):
+        try:
+            with open(configFile, 'w') as configfile:
+                self.conf.write(configfile)
+        except:
+            print("Error in saving parameters")
+
 
 
 #************************************
