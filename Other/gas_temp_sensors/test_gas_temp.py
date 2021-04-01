e82e29a8b3b7d792273a7695f2711acdb2cd97b8
Nicola Ferralis <feranick@hotmail.com>
2021-04-01 15:17:12 -0400

    Added new gas and temperature sensors using circuitPython

---
+++ Other/gas_temp_sensors/test_gas_temp.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+  
+import time
+import board
+import busio
+import adafruit_sgp30
+import adafruit_mcp9808
+ 
+i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
+ 
+# Create library object on our I2C port
+sgp30 = adafruit_sgp30.Adafruit_SGP30(i2c)
+# To initialise using the default address:
+mcp = adafruit_mcp9808.MCP9808(i2c)
+
+# To initialise using a specified address:
+# Necessary when, for example, connecting A0 to VDD to make address=0x19
+# mcp = adafruit_mcp9808.MCP9808(i2c_bus, address=0x19)
+ 
+ 
+print("SGP30 serial #", [hex(i) for i in sgp30.serial])
+sgp30.iaq_init()
+sgp30.set_iaq_baseline(0x8973, 0x8AAE)
+elapsed_sec = 0
+ 
+while True:
+    #print("eCO2 = %d ppm \t TVOC = %d ppb" % (sgp30.eCO2, sgp30.TVOC))
+    print("eCO2 = %d ppm \t TVOC = %d ppb \t TempC = %d" % (sgp30.eCO2, sgp30.TVOC, mcp.temperature))
+    time.sleep(1)
+    elapsed_sec += 1
+    if elapsed_sec > 10:
+        elapsed_sec = 0
+        print(
+            "**** Baseline values: eCO2 = 0x%x, TVOC = 0x%x"
+            % (sgp30.baseline_eCO2, sgp30.baseline_TVOC)
+        )
+
+
