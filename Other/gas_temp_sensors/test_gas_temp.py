81aa04eeb64d745b4f67ea0b454a4a769709228a
Nicola Ferralis <feranick@hotmail.com>
2021-04-03 22:38:45 -0400

    Update test_gas_temp.py

--- Other/gas_temp_sensors/test_gas_temp.py
+++ Other/gas_temp_sensors/test_gas_temp.py
@@ -5,13 +5,16 @@ import board
 import busio
 import adafruit_sgp30
 import adafruit_mcp9808
+import adafruit_bme280
  
 i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
  
 # Create library object on our I2C port
 sgp30 = adafruit_sgp30.Adafruit_SGP30(i2c)
 # To initialise using the default address:
-mcp = adafruit_mcp9808.MCP9808(i2c)
+#mcp = adafruit_mcp9808.MCP9808(i2c)
+mcp = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+
 
 # To initialise using a specified address:
 # Necessary when, for example, connecting A0 to VDD to make address=0x19
@@ -20,12 +23,13 @@ mcp = adafruit_mcp9808.MCP9808(i2c)
  
 print("SGP30 serial #", [hex(i) for i in sgp30.serial])
 sgp30.iaq_init()
-sgp30.set_iaq_baseline(0x8973, 0x8AAE)
+#sgp30.set_iaq_baseline(0x8973, 0x8AAE)
+sgp30.set_iaq_baseline(0x8cd4,0x982a)
 elapsed_sec = 0
  
 while True:
     #print("eCO2 = %d ppm \t TVOC = %d ppb" % (sgp30.eCO2, sgp30.TVOC))
-    print("eCO2 = %d ppm \t TVOC = %d ppb \t TempC = %0.1f" % (sgp30.eCO2, sgp30.TVOC, mcp.temperature))
+    print("eCO2 = %d ppm \t TVOC = %d ppb \t TempC = %0.1f" % (sgp30.eCO2, sgp30.TVOC, mcp.temperat>
     time.sleep(1)
     elapsed_sec += 1
     if elapsed_sec > 10:
@@ -34,5 +38,3 @@ while True:
             "**** Baseline values: eCO2 = 0x%x, TVOC = 0x%x"
             % (sgp30.baseline_eCO2, sgp30.baseline_TVOC)
         )
-
-
