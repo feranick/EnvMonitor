31da35ad22a2e1ff9a852e1aa136577bd1da7e37
Nicola Ferralis <feranick@hotmail.com>
2021-04-07 16:17:08 -0400

    Added launcher to allow EnvMonitorLite to run while logged off.

---
+++ EnvMonitor/EnvMonitorLite_launcher.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#####################################
+#
+# EnvMonitorLite launcher
+#
+# v. 20210407a
+#
+# Nicola Ferralis <feranick@hotmail.com>
+#
+#####################################
+
+nohup EnvMonitorLite > ~/EnvMonLog.txt 2>&1 &
