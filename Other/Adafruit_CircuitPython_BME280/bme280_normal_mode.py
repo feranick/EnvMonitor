4988eba59097d6d63907586eb55163caeaa1926c
Nicola Ferralis <feranick@hotmail.com>
2021-04-07 14:42:37 -0400

    Actually use BME280 test files

--- Other/Adafruit_CircuitPython_BME280/bme280_normal_mode.py
+++ Other/Adafruit_CircuitPython_BME280/bme280_normal_mode.py
@@ -2,37 +2,39 @@
 # SPDX-License-Identifier: MIT
 
 """
-Example showing how the BMP280 library can be used to set the various
+Example showing how the BME280 library can be used to set the various
 parameters supported by the sensor.
-Refer to the BMP280 datasheet to understand what these parameters do
+Refer to the BME280 datasheet to understand what these parameters do
 """
 import time
 
 import board
 import busio
-import adafruit_bmp280
+import adafruit_bme280
 
 # Create library object using our Bus I2C port
 i2c = busio.I2C(board.SCL, board.SDA)
-bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
 # OR create library object using our Bus SPI port
 # spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-# bmp_cs = digitalio.DigitalInOut(board.D10)
-# bmp280 = adafruit_bmp280.Adafruit_BMP280_SPI(spi, bmp_cs)
+# bme_cs = digitalio.DigitalInOut(board.D10)
+# bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
 
-# change this to match the location's pressure (hPa) at sea level
-bmp280.sea_level_pressure = 1013.25
-bmp280.mode = adafruit_bmp280.MODE_NORMAL
-bmp280.standby_period = adafruit_bmp280.STANDBY_TC_500
-bmp280.iir_filter = adafruit_bmp280.IIR_FILTER_X16
-bmp280.overscan_pressure = adafruit_bmp280.OVERSCAN_X16
-bmp280.overscan_temperature = adafruit_bmp280.OVERSCAN_X2
-# The sensor will need a moment to gather inital readings
+# Change this to match the location's pressure (hPa) at sea level
+bme280.sea_level_pressure = 1013.25
+bme280.mode = adafruit_bme280.MODE_NORMAL
+bme280.standby_period = adafruit_bme280.STANDBY_TC_500
+bme280.iir_filter = adafruit_bme280.IIR_FILTER_X16
+bme280.overscan_pressure = adafruit_bme280.OVERSCAN_X16
+bme280.overscan_humidity = adafruit_bme280.OVERSCAN_X1
+bme280.overscan_temperature = adafruit_bme280.OVERSCAN_X2
+# The sensor will need a moment to gather initial readings
 time.sleep(1)
 
 while True:
-    print("\nTemperature: %0.1f C" % bmp280.temperature)
-    print("Pressure: %0.1f hPa" % bmp280.pressure)
-    print("Altitude = %0.2f meters" % bmp280.altitude)
+    print("\nTemperature: %0.1f C" % bme280.temperature)
+    print("Humidity: %0.1f %%" % bme280.relative_humidity)
+    print("Pressure: %0.1f hPa" % bme280.pressure)
+    print("Altitude = %0.2f meters" % bme280.altitude)
     time.sleep(2)
