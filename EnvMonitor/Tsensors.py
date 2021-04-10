40112605ce48786307fd41956978827bc1185b44
Nicola Ferralis <feranick@hotmail.com>
2021-04-10 18:18:52 -0400

    FIx bugs

--- EnvMonitor/Tsensors.py
+++ EnvMonitor/Tsensors.py
@@ -85,9 +85,9 @@ class TRHSensor:
         
         Pws = Pc * math.exp((Tc/T)*(C1*nu + C2*pow(nu, 1.5) + C3*pow(nu, 3) + C4*pow(nu, 3.5) + C5*pow(nu, 4) + C6*pow(nu, 7.5)))    #  in hPa
         
-        self.RhA = C * (Pws * RH / 100) * 100 /(273.15 + T)
+        self.RhA = C * (Pws * RH / 100) * 100 /T
         
-        print(6.116441*pow(10, (7.591386*T)/(T+240.7263)))
+        print(6.116441*pow(10, (7.591386*T)/(T1+240.7263)))
         
         return self.RhA
                 
